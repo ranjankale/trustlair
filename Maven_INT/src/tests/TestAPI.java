f417b176396bc19a2013366c97fb35e8f08763d8
ranjan <ranjan.kale@gmail.com>
2018-12-29 22:23:43 +0530

    c2e

--- Maven_INT/src/tests/TestAPI.java
+++ Maven_INT/src/tests/TestAPI.java
@@ -3,17 +3,18 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class TestAPI {
 	
 
 	@Test
-	public void test()
+	public void test() throws Exception
 	{ 
 		String path=System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver",path+"\\resources\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.ie.driver",path+"\\resources\\IEDriverServer.exe");
+		WebDriver driver=new InternetExplorerDriver();
 		driver.get("http://www.google.com");
 		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Google"+"\n");
 		driver.close();
