1ee2a50c47dea027e7f645bd3676c9ca089d4462
ranjan <ranjan.kale@gmail.com>
2019-01-15 22:19:03 +0530

    commit A.java

--- Maven_INT/src/tests/A.java
+++ Maven_INT/src/tests/A.java
@@ -1,9 +1,25 @@
 package tests;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
 public class A {
 	
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	@Test(groups= {"t1","t2"})
+	public void t1() {
+		System.out.println("test1");
 	}
 
+	@Test(groups= {"t1"})
+	public void t2() {
+		System.out.println("test2");
+	}
+	@Test(groups= {"t1"})
+	public void t3() {
+		System.out.println("test3");
+	}
+	@Test
+	public void t4() {
+		System.out.println("test4");
+	}
 }
